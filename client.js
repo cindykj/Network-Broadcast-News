8f8a52c18713f504f9b0ca05a1455440ab62bce7
KanKan <cindykan@KanKan.local>
2018-01-23 19:28:28 -1000

    Server is taking in name, but clients not talking

--- client.js
+++ client.js
@@ -1,1 +1,25 @@
 const net = require('net');
+const PORT = 6969;
+const HOST = '0.0.0.0';
+
+const client = net.createConnection(PORT, HOST, () => { //creating new socket as init
+  // connect listener
+  console.log('Connected to server!');
+  client.setEncoding('utf8');
+  // client.write('Client just signed in.\r\n');
+  // write to server
+  process.stdin.pipe(client);
+
+  client.on('data', (data) => {
+    process.stdout.write(`${data}`); //as soon as you get data from server, then console.log
+    // client.end(); //this ends the connection from client side
+  }); //closing for client.on data
+  
+}); //closing for createConnection
+
+
+// to end connection
+client.on('end', () => {
+  console.log('disconnected from server');
+}); // closing for client.on end
+
--- server.js
+++ server.js
@@ -1,1 +1,62 @@
 const net = require('net');
+const PORT = 6969;
+const HOST = '0.0.0.0';
+let connectionCount = [];
+
+
+const server = net.createServer(function (socket) {
+  console.log('Client connected!');
+  socket.setEncoding('utf8');
+  process.stdin.pipe(socket); //pipe from server to socket
+  socket.username = null; // make property on socket object
+  console.log(connectionCount);
+
+  socket.write('Welcome, Client!\r\n Please enter your username: '); //carriage return, linefeed (a la typewriter)
+
+  socket.on('data', (data) => {
+    // console.log(data);
+    // if new user name, please set
+    if (socket.username === null) {
+      let username = socket.username;
+      if (!data.trim().includes('admin')) {
+        username = data.trim();
+        socket.write(`Welcome back, ${username}! \r\n`);
+        console.log(`${username} just logged in.`)
+      } else {
+        socket.write('Username cannot contain admin. Please try again.\r\n Please enter your username: ');
+      }
+      // console.log(connectionCount); 
+    } else {
+      connectionCount
+        .filter(element => {
+          return element !== socket; // filters whether incoming is true connection
+        })
+        .forEach(element => {
+          element.write(`${username}, ${data}`) // this writes msg to other connected clients
+        })
+    };
+  });
+
+  connectionCount.push(socket); //counts number of listeners
+
+  //function to end connection
+  socket.on('end', () => {
+    //cleanup to remove the disconnected socket
+    connectionCount.splice(connectionCount.indexOf(socket), 1)
+    console.log('Client disconnected');
+  }); //closing for socket.on(end)
+
+}); // closing server connect
+
+// socket.pipe(socket); //this pipes back to itself as echo (i.e. sanity check)
+// socket.end(); //this ends connection from server side
+
+// 
+server.on('error', (err) => {
+  throw err;
+});
+
+// Listens for connections on port 6969
+server.listen(PORT, () => {
+  console.log('Server listening on');
+}); // closing for server.listen
