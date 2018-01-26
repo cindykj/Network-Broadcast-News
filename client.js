b93151412e35ab3184b96fe530f266468671ac70
KanKan <cindykan@KanKan.local>
2018-01-26 13:52:31 -1000

    Clients are not talking to each other, nor is it on the server

--- client.js
+++ client.js
@@ -3,16 +3,13 @@ const PORT = 6969;
 const HOST = '0.0.0.0';
 
 const client = net.createConnection(PORT, HOST, () => { //creating new socket as init
-  // connect listener
   console.log('Connected to server!');
   client.setEncoding('utf8');
-  // client.write('Client just signed in.\r\n');
   // write to server
   process.stdin.pipe(client);
 
   client.on('data', (data) => {
-    process.stdout.write(`${data}`); //as soon as you get data from server, then console.log
-    // client.end(); //this ends the connection from client side
+    process.stdout.write(data); //as soon as you get data from server, then console.log
   }); //closing for client.on data
   
 }); //closing for createConnection
--- server.js
+++ server.js
@@ -8,24 +8,19 @@ const server = net.createServer(function (socket) {
   console.log('Client connected!');
   socket.setEncoding('utf8');
   process.stdin.pipe(socket); //pipe from server to socket
-  socket.username = null; // make property on socket object
-  console.log(connectionCount);
+  socket.username = undefined; // make property on socket object
 
-  socket.write('Welcome, Client!\r\n Please enter your username: '); //carriage return, linefeed (a la typewriter)
+  if (socket.username === undefined) {
+    socket.write('Welcome, Client!\r\n Please enter your username: '); //carriage return, linefeed (a la typewriter)
+  };
 
   socket.on('data', (data) => {
-    // console.log(data);
     // if new user name, please set
-    if (socket.username === null) {
+    if (!socket.username) {
       let username = socket.username;
-      if (!data.trim().includes('admin')) {
-        username = data.trim();
-        socket.write(`Welcome back, ${username}! \r\n`);
-        console.log(`${username} just logged in.`)
-      } else {
-        socket.write('Username cannot contain admin. Please try again.\r\n Please enter your username: ');
-      }
-      // console.log(connectionCount); 
+      username = data.trim();
+      socket.write(`Welcome back, ${username}! \r\n`);
+      socket.write(`${username} just logged in.`)
     } else {
       connectionCount
         .filter(element => {
@@ -37,26 +32,34 @@ const server = net.createServer(function (socket) {
     };
   });
 
+  
   connectionCount.push(socket); //counts number of listeners
-
+  
   //function to end connection
   socket.on('end', () => {
     //cleanup to remove the disconnected socket
     connectionCount.splice(connectionCount.indexOf(socket), 1)
     console.log('Client disconnected');
   }); //closing for socket.on(end)
-
+  
 }); // closing server connect
 
-// socket.pipe(socket); //this pipes back to itself as echo (i.e. sanity check)
-// socket.end(); //this ends connection from server side
 
-// 
 server.on('error', (err) => {
   throw err;
 });
 
 // Listens for connections on port 6969
-server.listen(PORT, () => {
+server.listen(PORT, HOST, () => {
   console.log('Server listening on');
 }); // closing for server.listen
+
+
+// admin log in
+// if (!data.trim().includes('admin')) {
+  // } else {
+    //   socket.write('Username cannot contain admin. Please try again.\r\n Please enter your username: ');
+    // }
+
+//this pipes back to itself as echo (i.e. sanity check)
+    // socket.pipe(socket); 
